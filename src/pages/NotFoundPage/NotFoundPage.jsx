2c052b504e88620bfe2b10346d00e3498435a19c
andrewwwmatsko <matskoandrew@gmail.com>
2024-07-12 03:29:54 +0300

    404 page + autoredirect

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,15 +1,42 @@
+import { Link, useNavigate } from "react-router-dom";
 import imgUrl from "../../images/404.png";
 
 import css from "./NotFoundPage.module.css";
+import { useEffect, useState } from "react";
 
 export default function NotFoundPage() {
-  setTimeout(() => {}, 3000);
+  const [calldown, setCalldown] = useState(3);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCalldown((prev) => prev - 1);
+
+      if (calldown < 1) {
+        navigate("/", { replace: true });
+        clearInterval(intervalId);
+        return;
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [calldown, navigate]);
 
   return (
     <main>
       <section className={css.section}>
         <div className={css.container}>
-          <img src={imgUrl} alt="404 error" width={900} />
+          <img src={imgUrl} alt="404 error" width={900} className={css.img} />
+          <p className={css.text}>Page was not found.</p>
+          <p className={css.linkText}>
+            You are being redirected to the
+            <Link to="/" className={css.link}>
+              Home Page
+            </Link>
+            after {calldown} seconds
+          </p>
         </div>
       </section>
     </main>
